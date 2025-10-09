48c3ae1b42869008a7ee2489ddfacd786f8455a8
sathishps58 <sathishps58@gmail.com>
2025-10-09 09:25:31 +0530

    Java programs practice

--- src/Prime_Number_Check.java
+++ src/Prime_Number_Check.java
@@ -1,3 +1,5 @@
+// Java Program of Prime Number using function declaration
+
 package src;
 
 import java.util.Scanner;
@@ -30,5 +32,41 @@ public class Prime_Number_Check {
     }
 }
 
+// Java Program of Prime Number without using function declaration
 
+/*import java.util.Scanner;
 
+public class PrimeNumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // To take input from user
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt(); // Read the number
+
+        boolean isPrime = true; // Assume the number is prime first
+
+        // Prime numbers are greater than 1
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            // Check if num is divisible by any number from 2 to num-1
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) { // if divisible, not prime
+                    isPrime = false;
+                    break; // no need to check further
+                }
+            }
+        }
+
+        // Display the result
+        if (isPrime) {
+            System.out.println(num + " is a Prime Number.");
+        } else {
+            System.out.println(num + " is NOT a Prime Number.");
+        }
+
+        sc.close(); // Close the scanner
+    }
+}
+
+ */
